1e8cd412e279f7d9a84a84d56d037426e84bca7a
SantoshDattatryPatil <41010464+SantoshDattatryPatil@users.noreply.github.com>
2020-10-10 10:36:34 +0530

    Initial commit

---
+++ src/test/java/com/santosh/redit/Redit/Clone/ReditCloneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.santosh.redit.Redit.Clone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReditCloneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
